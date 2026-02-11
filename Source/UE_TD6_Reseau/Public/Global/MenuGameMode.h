7ebd21cdd6a8c8b2e10ea2dba3e0f5e1bdfdad8d
Liam Duret <115427960+Poyo1300@users.noreply.github.com>
2026-02-11 16:10:10 +0100

    Added Menu
    Main Menu + lobby window
    Lobby Menu

---
+++ Source/UE_TD6_Reseau/Public/Global/MenuGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MenuGameMode.generated.h"
+
+UCLASS()
+class UE_TD6_RESEAU_API AMenuGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UE_TD6_Reseau/Public/Player/MenuController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MenuController.generated.h"
+
+UCLASS()
+class UE_TD6_RESEAU_API AMenuController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
