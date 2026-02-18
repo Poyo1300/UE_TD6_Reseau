0912a9efd26cbbc335b930ed133101107904d2b5
Xima <74795630+XimaLc@users.noreply.github.com>
2026-02-18 18:27:28 +0100

    TrapButton BaseTrap link

---
+++ Source/UE_TD6_Reseau/Private/Actors/Traps/BaseTrap.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Traps/BaseTrap.h"
+
+// Sets default values
+ABaseTrap::ABaseTrap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root Component");
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("Static mesh component");
+	StaticMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ABaseTrap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseTrap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseTrap::ActivateTrap()
+{
+	GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, "Trap activated");
+}
+
---
+++ Source/UE_TD6_Reseau/Public/Actors/Traps/BaseTrap.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseTrap.generated.h"
+
+UCLASS()
+class UE_TD6_RESEAU_API ABaseTrap : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseTrap();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void ActivateTrap();
+};
