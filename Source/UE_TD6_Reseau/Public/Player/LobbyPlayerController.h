e07b89199457e0b09a22683d331afca7e36c8e2e
Liam Duret <115427960+Poyo1300@users.noreply.github.com>
2026-02-19 15:58:21 +0100

    lobby player character

---
+++ Source/UE_TD6_Reseau/Public/Player/LobbyPlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LobbyPlayerController.generated.h"
+
+UCLASS()
+class UE_TD6_RESEAU_API ALobbyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
